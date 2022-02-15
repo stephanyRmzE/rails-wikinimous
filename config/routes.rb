323407eb6db2aed5c1be332a6685d7a39fdbb759
stephany ramirez <stephany_re@hotmail.com>
2022-02-15 08:22:59 -0700

    i need to do the css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  delete '/articles/:id', to: 'articles#destroy'
 end
